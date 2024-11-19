9b635e0f4459222447cd2f54cf07bbfbdf7a8d35
nerenber26 <112669015+nerenber26@users.noreply.github.com>
2024-11-18 23:10:17 -0800

    Updated Boxes
    Now when drawing the rectangles spacebar presses are required to draw each subsequent one so that students can see each rectangle as it appears, reducing the chance they get confused if a rectangle is drawing over another.

--- Boxes.java
+++ Boxes.java
@@ -5,15 +5,17 @@
 // Import a couple of library-packages
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 /** class Boxes
  *
  * The important thing that we need to do in this class is finish the
  * 'paint' method.  
  */
-public class Boxes extends Canvas
+public class Boxes extends Canvas implements KeyListener
 {
-
+    int clickCount = 0;
     /** the "paint" method, that specifies what to draw on the screen
      * 
      * This method needs to be updated
@@ -23,15 +25,54 @@ public class Boxes extends Canvas
         //This line is important, don't remove it!
         super.paint(g);
         
-        //These statements draw some boxes, but they are not the boxes you want.
+        //Don't touch these ones either
+        Font originalFont = g.getFont();
+        Font newFont = originalFont.deriveFont(originalFont.getSize2D() * 1.4f);
+        g.setFont(newFont);
+        g.drawString("Press space to draw all rectangles...", 0, 18);
+        
+        //The statements below draw some boxes, but they aren't the boxes you want.
         //Replace them with the correct code.
-        g.setColor(Color.red);
-        g.fillRect(100,100,100,100);
-        g.setColor(Color.green);
-        g.fillRect(200,200,100,100);
+        
+        //Don't edit the if statements themselves, just edit the lines inside
+        //of them that actually make the rectangles
+        
+        //When you are running the program, press the spacebar to draw 
+        //the next rectangles
+        if (clickCount >= 0) {
+            g.setColor(Color.red);
+            g.fillRect(100,100,100,100);
+        } 
+        if (clickCount >= 1) {
+            g.setColor(Color.green);
+            g.fillRect(200,200,100,100);
+        }
+        if (clickCount >= 2) {
+            g.setColor(Color.green);
+            g.fillRect(300,300,100,100);
+        }
+        
+      
         
     }//paint
 
+     @Override
+    public void keyTyped(KeyEvent e) {
+        // Ignore key typed events
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            clickCount++;
+            repaint();
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // Ignore key released events
+    }
     /**
      * main
      *
@@ -55,6 +96,8 @@ public class Boxes extends Canvas
         // display the frame
         myFrame.setVisible(true);
         
+        myContent.addKeyListener(myContent);
+        myContent.setFocusable(true); // Ensure the canvas receives focus
     }//main
 
 
